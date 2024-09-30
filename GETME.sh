a1ea63546f2b3cf40e436115f5c22c2216e9ed08
abel <roffiaen.abel@gmail.com>
2024-09-30 10:11:35 +0200

    Patch install

--- GETME.sh
+++ GETME.sh
@@ -7,7 +7,7 @@ git clone git@github.com:abelr-githubplace/epita_format.git
 cd ./epita_format/
 cargo b -rq
 cp ./target/release/epita_format ~/afs/
-cp ./clang-format ~/afs/
+cp ./.clang-format ~/afs/
 cp ./README ~/afs/
 cd ..
 rm -rf epita_format/
